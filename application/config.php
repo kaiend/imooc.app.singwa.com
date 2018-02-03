d2ff62c82da44ef52f32f312e8ebc0677848b102
kaiend <kaiend@126.com>
2018-02-03 11:12:07 +0800

    captcha

--- application/config.php
+++ application/config.php
@@ -236,4 +236,14 @@ return [
         'var_page'  => 'page',
         'list_rows' => 15,
     ],
+    'captcha' =>   [
+        // 验证码字体大小
+        'fontSize'    =>    20,
+        // 验证码位数
+        'length'      =>    3,
+        // 关闭验证码杂点
+        //'useNoise'    =>    false,
+        'imageH'    => 41,
+        'imageW'     => 200,
+    ],
 ];
