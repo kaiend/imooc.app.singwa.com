98601fe526bb9d8571435208c1c8660c4269bb74
kaiend <kaiend@126.com>
2018-03-15 13:40:32 +0800

    3-15 新闻详情页面接口开发

--- application/api/controller/v1/News.php
+++ application/api/controller/v1/News.php
@@ -10,6 +10,7 @@ namespace app\api\controller\v1;
 
 
 use app\api\controller\Common;
+use app\common\lib\exception\ApiException;
 
 class News extends Common
 {
@@ -36,6 +37,37 @@ class News extends Common
             'list' => $this->getDealNews($news)
         ];
         return show(config('code.success'), 'ok', $result, 200);
+    }
+
+    /**
+     * 獲取詳情頁接口
+     */
+    public function read()
+    {
+        //詳情頁面 app-》 1，x.com/
+        $id = input('param.id', 0, 'intval');
+        if (empty($id)) {
+            return new ApiException('ID IS NOT', 404);
+        }
+
+        //通過id獲取數據
+        $news = model('News')->get($id);
+        if (empty($news) || $news->status != config('code.status_normal')) {
+            return new ApiException('不存在改新聞', 404);
+        }
+
+        //閱讀數自動加1
+        try {
+            model('News')->where(['id' => $id])->setInc('read_count');
+
+        } catch (\Exception $e) {
+            return new ApiException('ERROR不存在的新聞', 400);
+        }
+
+        $cats = config('cat.list');//獲取欄目名稱
+        $news->catname = $cats[$news->catid];//獲取欄目名稱
+        return show(config('code.success'), 'ok', $news, 200);
+
     }
 
 }
